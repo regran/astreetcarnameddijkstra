ba37d6045a9b4a5ed9bb949d1e0dab8957e6107e
Brian <brncherin@gmail.com>
2017-03-26 04:35:41 -0400

    Find adjacent squares for a given square

--- graphics.py
+++ graphics.py
@@ -28,21 +28,42 @@ green = (76, 175, 80)
 lightgreen=(139, 195, 74)
 lime = (205, 220, 57)
 pink = (233, 30, 99)
-#itemcolors=[white, brown, blue, green, lightgreen]
+gray = (96, 125, 139)
 
 
 class Square(object):
+
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		
+	# try:
+		# adjacent = [squarelist[x][y+1], squarelist[x+1][y+1], squarelist[x+1][y], squarelist[x+1][y-1], squarelist[x][y+1], squarelist[x-1][y-1], squarelist[i][j], squarelist[i-1][y+1]]
+	# except Exception as error:
+		# #print()
+		# print('caught this error:' + repr(error))
 	
 	def draw(cls, surface, x, y):
 		s = pygame.Surface((squareWidth, squareWidth))
 		s.fill(cls.color)
 		surface.blit(s, (x, y))
 	
+	def getAdjacent(cls):
+		x = cls.x
+		y = cls.y
+		return [squarelist[x][y+1], squarelist[x+1][y+1], squarelist[x+1][y], squarelist[x+1][y-1], squarelist[x][y+1], squarelist[x-1][y-1], squarelist[i][j], squarelist[i-1][y+1]]
+	
+	 
+class Border(Square):
+	color = (0, 0, 0)
+	type = -1
+	cost = 100000
+	isDestination = False
 	 
 class Blank(Square):
 	color = lightgreen
 	type = 0
-	cost = 1
+	cost = 2
 	isDestination = False
 		
 
@@ -68,14 +89,26 @@ class Park(Square):
 class Forest(Square):
 	color = lime
 	type = 4
-	cost = 3
+	cost = 4
 	isDestination = False
 
 class Water(Square):
 	color = blue
 	type = 5
-	cost = 3
+	cost = 4
 	isDestination = False
+	
+class Road(Square):
+	color = gray
+	type = 6
+	cost = 1
+	isDestination = False
+
+# class Person (pygame.sprite.sprite):
+		# def __init__(self):
+			# pygame.sprite.Sprite.__init__(self)
+			# self.image.fill(0, 255, 0)
+			# self.rect = self.image.get_rect()	
 
 
 
@@ -93,23 +126,38 @@ probforest = .05
 probwater = .02	
 weights = [probblank, probhouse, probstore, probpark, probforest, probwater]	#probabilities of each square type
 squarelist = [[choice(elements, p=weights) for i in range(squaresPerRow)] for j in range(squaresPerRow)]	#generate 2d array of square types
+#print (squarelist)
+for row in squarelist:
+	print (row)
+
+#Add border to map
+squarelist = [[-1 for i in range(len(squarelist))]] + squarelist + [[-1 for i in range (len(squarelist))]]
+
+for i in range (len(squarelist)):
+	squarelist[i] = [-1] + squarelist[i] + [-1]
+
 print (squarelist)
+
 
 #replaces numerical types with actual type objects
 for i in range(len(squarelist)):
 	for j in range(len(squarelist[i])):
 		if squarelist[i][j] == 0:
-			squarelist[i][j]=(Blank())
+			squarelist[i][j]=(Blank(i, j))
 		elif squarelist[i][j] == 1:
-			squarelist[i][j]=(House())
+			squarelist[i][j]=(House(i, j))
 		elif squarelist[i][j] == 2:
-			squarelist[i][j]=(Store())
+			squarelist[i][j]=(Store(i, j))
 		elif squarelist[i][j] == 3:
-			squarelist[i][j]=(Park())
+			squarelist[i][j]=(Park(i, j))
 		elif squarelist[i][j] == 4:
-			squarelist[i][j]=(Forest())
+			squarelist[i][j]=(Forest(i, j))
 		elif squarelist[i][j] == 5:
-			squarelist[i][j]=(Water())
+			squarelist[i][j]=(Water(i, j))
+		elif squarelist[i][j] == 6:
+			squarelist[i][j]=(Road(i, j))
+		elif squarelist[i][j] == -1:
+			squarelist[i][j]=(Border(i, j))
 #print (squarelist)
 
 
@@ -132,13 +180,18 @@ def main():
 	background.blit(text, textpos)
 	
 	#Display all the squares
-	for i in range (len(squarelist)):
-		for j in range (len(squarelist[i])):
-			x = j * squareWidth
-			y = i * squareWidth
+	for i in range (1,len(squarelist)-1):
+		for j in range (1, len(squarelist[i])-1):
+			x = (j-1) * squareWidth
+			y = (i-1) * squareWidth
 		#	print (squarelist[i][j])
 			squarelist[i][j].draw(background, x, y)
+			#print(squarelist[i][j].adjacent)
+			print (squarelist[i][j].getAdjacent())
 
+			
+	
+			
 	#Blit everything to the screen
 	screen.blit(background, (0, 0))
 	pygame.display.flip()
