4fb68b6fb6743ef33f81cf9a605fd0f80379e0c3
Reggy G <rgranovskiy@gmail.com>
2017-03-26 01:08:35 -0400

    Fixed heap

--- heap.py
+++ heap.py
@@ -1,66 +1,61 @@
 class pHeap:
-	def __init__(self):		#constructs new tree array
-		space = 0 #next empty space
-		a = [[]]    #tree array
+    def __init__(self):		#constructs new tree array
+        self.space = 0 #next empty space
+        self.a = []    #tree array
 
-	def push(self, s, d):		#adds s in order with smallest d at top
-		swap = [0,0]	#holder for swaps
-		check = space		#hold location of s
-		a[space][0]=s			#add at space
-		a[space][1]=d
-		while(check!=0 and a[check][1]<a[(check-1)/2][1]): #carry s up heap until it's ordered
-			swap[0]=a[check][0]
-			swap[1]=a[check][1]
-			a[check][0]=a[(check-1)/2][0]
-			a[check][1]=a[(check-1)/2][1]
-			if(check/2!=0):
-				a[check/2-1][0]=swap[0]
-				a[check/2-1][1]=swap[1]
-			
-			else: 
-				a[check/2][0]=swap[0]
-				a[check/2][1]=swap[1]
-			check=(check-1)/2
-		space+=1	#move to next space	
+    def push(self, s, d):		#adds s in order with smallest d at top
+        swap = [0,0]	#holder for swaps
+        check = self.space		#hold location of s
+        self.a+=[[s,d]]			#add at space
+        while(check!=0 and self.a[check][1]<self.a[int((check-1)/2)][1]): #carry s up heap until it's ordered
+            swap[0]=self.a[check][0]
+            swap[1]=self.a[check][1]
+            self.a[check][0]=self.a[int((check-1)/2)][0]
+            self.a[check][1]=self.a[int((check-1)/2)][1]
+            if(check/2!=0):
+                self.a[int(check/2)-1][0]=swap[0]
+                self.a[int(check/2)-1][1]=swap[1]	
+            else: 
+                self.a[int(check/2)][0]=swap[0]
+                self.a[int(check/2)][1]=swap[1]
+            check=int((check-1)/2)
+        self.space+=1	#move to next space	
 
-		
-	def pop(self):
-		swap = [0,0] 	#holder for swaps
-		r=a[0][0]			#hold return data			
-                #check holds location of thing being ordered
-		if(isEmpty()):			#check if there's something to pop
-			return -1
+    def pop(self):
+        swap = [0,0] 	#holder for swaps
+        r=self.a[0][0]			#hold return data			
+        #check holds location of thing being ordered
+        if(self.isEmpty()):			#check if there's something to pop
+            return -1
+        self.space=self.space-1		#move space back
+        self.a[0][0]=self.a[self.space][0]		#move last element to root
+        self.a[0][1]=self.a[self.space][1]
+        self.a.pop()	#make space empty
+        check=0
+        while(check<self.space and check*2+1<self.space and (self.a[check][1]>self.a[check*2+1][1] or(check*2+2<self.space and self.a[check*2+2][1]<self.a[check][1]))):
+	#go down heap and swap until ordered
+            if(self.a[check*2+2][1]>self.a[check*2+1][1] or self.space==check*2+2):	#move smaller element up
+                swap[0]=self.a[check][0]
+                swap[1]=self.a[check][1]
+                self.a[check][0]=self.a[check*2+1][0]
+                self.a[check][1]=self.a[check*2+1][1]
+                self.a[check*2+1][0]=swap[0]
+                self.a[check*2+1][1]=swap[1]
+                check=check*2+1
+            else:
+                swap[0]=self.a[check][0]
+                swap[1]=self.a[check][1]
+                self.a[check][0]=self.a[check*2+2][0]
+                self.a[check][1]=self.a[check*2+2][1]
+                self.a[check*2+2][0]=swap[0]
+                self.a[check*2+2][1]=swap[1]
+                check=check*2+2
+        return r
 
-		space=space-1		#move space back
-		a[0][0]=a[space][0]		#move last element to root
-		a[0][1]=a[space][1]
-		a[space][0]=a[space+1][0]	#make space empty
-		a[space][1]=a[space+1][1]
-		check=0
-		while(check<space and check*2+1<space and (a[check][1]>a[check*2+1][1] or a[check*2+2][1]<a[check][1])):
-		#go down heap and swap until ordered
-			if(a[check*2+2][1]>a[check*2+1][1] or space==check*2+2):	#move smaller element up
-				swap[0]=a[check][0]
-				swap[1]=a[check][1]
-				a[check][0]=a[check*2+1][0]
-				a[check][1]=a[check*2+1][1]
-				a[check*2+1][0]=swap[0]
-				a[check*2+1][1]=swap[1]
-				check=check*2+1
-			else:
-				swap[0]=a[check][0]
-				swap[1]=a[check][1]
-				a[check][0]=a[check*2+2][0]
-				a[check][1]=a[check*2+2][1]
-				a[check*2+2][0]=swap[0]
-				a[check*2+2][1]=swap[1]
-				check=check*2+2
+    def inspect(self):			#peak at top
+        if(self.isEmpty()): return -1
+        return self.a[0][0]
 
-		return r
-	def inspect(self):			#peak at top
-		if(isEmpty()): return -1
-		return a[0][0]
+    def isEmpty(self):		#check if empty
+        return self.space==0
 
-	def isEmpty(self):		#check if empty
-		return space==0
-	
